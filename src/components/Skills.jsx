097fa1d82d22a529d7e5f1a13a32252ee786f62c
Takahiro Aikou <ta-aikou@heart-musen.com>
2022-06-08 23:44:12 +0900

    useState, axios

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 export const Skills = () => {
+  const [languageList, setLanguageList] = useState([]);
+  console.log(languageList);
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/tasat31/repos')
+      .then((response) => {
+        const languageList = response.data.map(res => res.language)
+        const countedLanguageList = generateLanguageCountObj(languageList);
+        setLanguageList(countedLanguageList);
+      })
+    }, []);
+
+  const generateLanguageCountObj = (allLanguageList) => {
+    const notNullLanguageList = allLanguageList.filter(language => language != null);
+    const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+    return uniqueLanguageList.map(item => {
+      return {
+        language: item,
+        count: allLanguageList.filter(language => language === item).length
+      }
+    });
+  };
+
   return (
     <div id="skills">
       <div className="container">
