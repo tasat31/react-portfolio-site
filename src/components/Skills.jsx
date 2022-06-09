d2c8b3db6eb66d446ad4b5717d7ac00d6442ae7d
Takahiro Aikou <ta-aikou@heart-musen.com>
2022-06-09 22:27:08 +0900

    useReducer

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
+import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
 
 export const Skills = () => {
-  const [languageList, setLanguageList] = useState([]);
-  console.log(languageList);
+  const [state, dispatch] = useReducer(skillReducer, initialState);
 
   useEffect(() => {
+    dispatch({ type: actionTypes.fetch })
     axios.get('https://api.github.com/users/tasat31/repos')
       .then((response) => {
         const languageList = response.data.map(res => res.language)
         const countedLanguageList = generateLanguageCountObj(languageList);
-        setLanguageList(countedLanguageList);
+        dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
+      }).catch(() => {
+        dispatch({ type: actionTypes.error });
       })
     }, []);
 
