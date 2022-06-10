936559284fa58216e97f3e98374f823e9c939cbd
Takahiro Aikou <ta-aikou@heart-musen.com>
2022-06-10 12:45:41 +0900

    react-circle, useReducer

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,8 @@
+import Circle from 'react-circle';
 import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
+import { requestStates } from '../constants';
 
 export const Skills = () => {
   const [state, dispatch] = useReducer(skillReducer, initialState);
@@ -29,6 +31,15 @@ export const Skills = () => {
     });
   };
 
+  const converseCountToPercentage = (count) => {
+    if (count > 10) { return 100;}
+    return count * 10;
+  }
+
+  const sortedLanguageList = () => (
+    state.languageList.sort((firstLang, nextLang) => nextLang.count - firstLang.count)
+  )
+
   return (
     <div id="skills">
       <div className="container">
@@ -36,6 +47,29 @@ export const Skills = () => {
           <h2>Skills</h2>
         </div>
         <div className="skills-container">
+          {
+            state.requestState === requestStates.loading && (
+              <p className="description">取得中...</p>
+            )
+          }
+          {
+            state.requestState === requestStates.success && (
+              sortedLanguageList().map((item, index) => (
+                <div className="skill-item" key={index}>
+                  <p className="description"><strong>{item.language}</strong></p>
+                  <Circle
+                    animate
+                    progress={converseCountToPercentage(item.count)}
+                  />
+                </div>
+              ))
+            )
+          }
+          {
+            state.requestState === requestStates.error && (
+              <p className="description">エラーが発生しました</p>
+            )
+          }
         </div>
       </div>
     </div>
